0954615f4b0a82196993de8c45ec7160bb585a1f
Gilbert King <gilbertking91@gmail.com>
2020-08-22 14:51:55 -0500

    package prune

--- ingest.py
+++ ingest.py
@@ -3,16 +3,17 @@ import covid
 
 
 conn = psycopg2.connect(dbname="covid_data", 
-                        user="postgres", password="postgres", port=5433)
+                        user="postgres", password="postgres", port=5432)
 conn.set_session(autocommit=True)
 
 cur = conn.cursor()
 sql = "Insert into covid_data  (hash, date, states, positive, hostpitalizedCumulative, recovered, death,lastModified, total, deathIncrease) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" 
 
+# YOU NEED TO LOOK UP HOW TO INSERT these records PROPERLY 
 
 cur.execute(sql)
 
-cur.close()
+cur.close(sql)
 
-conn.close()
+conn.close(sql)
 
