00440633316b729d3d7c0ba3c15d19a4e87665cc
Gilbert King <gilbertking91@gmail.com>
2020-08-28 12:05:50 -0500

    cleaned aug 26 data

--- covid/covid.py
+++ covid/covid.py
@@ -11,21 +11,42 @@ cov = data
 
 
 
-#TRANSFORMS PYTHON DICT INFO AND PORTS INTO DAILY CSV FILE
+#TRANSFORMS PYTHON DICT INFO AND PORTS INTO DAILY CSV FILE also creates copy
 def to_csv(cov):
     CSV_URL = 'https://api.covidtracking.com/v1/us/current.csv'
     req = requests.get(CSV_URL)
     url_content = req.content
     csv_file = open('new_data.csv', 'wb')
     #PERHAPS USE .writerow() here
-    csv_file.write(url_content)
+    csv_file.writerows(url_content)
     csv_file.close()
-to_csv(cov)
+#to_csv(cov) 
 
-# EITHER WRITE AN ADDITIONAL FUNCTION TO JUST ADD A NEW ROW TO THE CSV
-#OR ADD THIS CAPABILITY INTO THE ABOVE FUNCTION ( I'D ADD IT INTO ABOVE FUNCTION )
+# THE UPDATE FUNCTION WORKS AND UPDATES THE LOG FILE
+def to_string(cov):
+    with open('new_info.csv', 'wb'):
+        #csv_writer = newt
+        CSV_URI = 'https://api.covidtracking.com/v1/us/current.csv'
+        reg = requests.get(CSV_URI)
+        new_line = reg.content
+        new_line.writerows(new_line)
+        new_line.close()
+#to_string(cov)
 
 
+
+
+with open('log.csv', 'a', newline='') as fd:
+    fieldnames = ['date','states','positive','negative','pending',
+              'hospitalizedCurrently','hospitalizedCumulative','inIcuCurrently',
+              'inIcuCumulative','onVentilatorCurrently','onVentilatorCumulative',
+              'recovered','dateChecked','death','hospitalized','lastModified','total',
+              'totalTestResults','posNeg','deathIncrease','hospitalizedIncrease',
+              'negativeIncrease','positiveIncrease','totalTestResultsIncrease','hash'
+              ]
+    writer = csv.DictWriter(fd, fieldnames=fieldnames)
+    writer.writerow(cov)
+    
 
 
     
